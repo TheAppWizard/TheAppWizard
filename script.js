8cc69aa60921d8053aa915d7dd991d611da56f65
Shreyas Bhike <70090469+TheAppWizard@users.noreply.github.com>
2022-02-27 18:41:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Engineer","Developer", "Blogger", "Designer", "Freelancer","TheAppWizard"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
